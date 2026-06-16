5a25ee5ab80f4086ec9bb7ea36bfbb2819763b91
BestBeast1 <127163930+BestBeast1@users.noreply.github.com>
2026-06-16 15:31:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 //  Get them from: https://supabase.com → your project → Settings → API
 // ─────────────────────────────────────────────────
 
-const SUPABASE_URL      = 'https://jipbclmcrynhurlsprot.supabase.co/rest/v1';       
+const SUPABASE_URL      = 'https://jipbclmcrynhurlsprot.supabase.co';       
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppcGJjbG1jcnluaHVybHNwcm90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1OTk4NzQsImV4cCI6MjA5NzE3NTg3NH0.OQvUajEAlCJ7t4dKw60xutAbPNlsQCF_TmhVLF_KcYU'; 
